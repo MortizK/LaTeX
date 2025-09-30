102aca7511ae409697532dcfb3319daa99466673
moritz <moritz-koehler@freenet.de>
2025-09-30 15:55:06 +0200

    Assembly Testing für RISC-V

---
+++ DHBW/sem3/Archi/assembly/test.s
@@ -0,0 +1,6 @@
+# Subtrahiert x6 von x0, d.h. negiert x6
+sub x5, x0, x6
+# Prüft, ob x0 kleiner als x13 ist, d.h. ob x13 größer als 0 ist
+slt x2, x0, x13
+# Speichert die Addition von 0 und x0 in x0; kanonisches NOP
+addi x0, x0, 0
